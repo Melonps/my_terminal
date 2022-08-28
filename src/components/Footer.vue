f6510160d8413b7a7cf501a2a138419c28d5e954
Melops <95263124+Melonps@users.noreply.github.com>
2022-08-28 16:06:35 +0900

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="footer">
+    <div class="access_top">
+  <!--    <router-link to="/top">
+        <p>
+          <img alt="nmu_brass ico" src="../assets/_nmu_brass_ico.png" />
+          <h5>Osaka Metropolitan University <br /> Brass Band </h5>
+          <h6>大阪公立大学吹奏楽部<h6>
+        </p>
+      </router-link> -->
+    </div>
+    <div id="menu">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/top">Top</router-link>
+    </div>
+  </div>
+</template>
+
+<style>
+#footer {
+background-color:#E82121;
+}
+#footer a {
+color:#FFFFFF;
+}
+</style>
---
+++ src/views/Top.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="top">
+  <!--
+    <img alt="nmu_brass logo" src="../assets/_nmu_brass_logo.png" />
+    -->
+    <h1>Welcome back!</h1>
+    <!--
+    <Topics />
+    <SocialLink />
+    -->
+  </div>
+</template>
+
+<script>
+
+//import Topics from "@/components/Topics.vue":
+//import SocialLinks from "@/components/SocialLinks.vue":
+
+export default {
+  name: "Top"
+//  components: {
+//    Topics,
+//    SocialLiks,
+//  },
+};
+</script>
+
