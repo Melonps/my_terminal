850758d8aac7ae4ff70add3f59f59926f6441247
Melops <95263124+Melonps@users.noreply.github.com>
2022-08-30 01:51:39 +0900

    [ADD] componentの作成 #1
    動作も確認しました、ここから時計のコンポーネントを作成しまつ

---
+++ src/components/Clock.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="clock">
+        <button id="btn1" v-on:click="countUp" class="ui teal button">実行</button>
+        <p>{{ count }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        countUp: function() {
+            // dataオプションで定義したcuontにアクセス
+            return this.count++
+        } 
+    }
+}
+</script>
+
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>abouts</h1>
+    <Clock></Clock>
   </div>
 </template>
+
+<script>
+
+import Clock from "@/components/Clock.vue";
+
+export default {
+  name: "about",
+  components: {
+    Clock,
+  },
+};
+</script>
