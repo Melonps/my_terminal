13a6f2c98c3c800a3e1d100514d4f75f03e1bfdc
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-02 16:28:31 +0900

    [ADD] とりあえずGoogle Searchを実装した

---
+++ src/components/GoogleSearch.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="gsearch">
+        <input ref="input"
+            title="Site Search"
+            v-on:keyup.enter = "search(textInput)"
+            v-model = "textInput" />
+        <button @click="search(textInput)">
+            Search
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'site'
+    ],
+    data() {
+        return {
+            textInput: ''
+        }
+    },
+    methods: {
+        search(str) {
+            if (str) {
+                window.open([
+                    'https://www.google.com/search?q=',
+                    this.site ? 'site:' + this.site + '+' : '',
+                    encodeURIComponent(str)
+                ].join(''));
+            } else {
+                this.$refs.input.focus();
+            }
+        }
+    }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>abouts</h1>
+    <GoogleSearch></GoogleSearch>
   </div>
 </template>
+
+<script>
+
+import GoogleSearch from "@/components/GoogleSearch.vue";
+
+export default {
+  name: "about",
+  components: {
+    GoogleSearch,
+  },
+};
+</script>
