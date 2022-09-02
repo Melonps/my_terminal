68920042b691e9d6e9b483d0bd7e05d302359fae
Melops <95263124+Melonps@users.noreply.github.com>
2022-09-02 20:32:20 +0900

    [ADD] コンポーネント自体の追加、簡単なショートカットの追加 #5
    とりあえず動くものになったはず、、、

---
+++ src/components/ShortcutFolder.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown button
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <div v-for="info of linkdata.group1" v-bind:key="info">
+                <img src="http://www.google.com/s2/favicons?domain= + info.url" alt="test"/>
+                <a :href="info.url" class="link-primary">{{info.name}}</a>
+            </div>
+        </ul>
+    </div>
+</template>
+
+<script>
+import shortcut from '../assets/user_data/shortcut.json';
+    console.log(shortcut);
+    export default {
+        data() {
+            return {
+                linkdata:shortcut.Groups,
+            };
+        }
+    };
+</script>
+
+<style scoped>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>abouts</h1>
+    <Shortcut></Shortcut>
   </div>
 </template>
+
+<script>
+import Shortcut from "@/components/ShortcutFolder.vue";
+export default {
+  name: "about",
+  components: {
+    Shortcut,
+  },
+};
+</script>
