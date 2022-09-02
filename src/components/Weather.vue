0ced90f70c93b6fdb18f131d5c0ed4764e58ebb6
Melops <95263124+Melonps@users.noreply.github.com>
2022-09-03 00:48:27 +0900

    [FIX] 動かないのでとりあえずコミット #2
    やりたいことは明示しました

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -6,26 +6,29 @@
 
 <script>
 let url = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json";
-let data;
-
-fetch(url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(weather) {
-        console.log(weather);
-        console.log(weather[0]);
-        data = weather[0].publishingOffice;
-        console.log(data);
-    });
-
 
 export default ({
     data: function () {
         return {
-            data:"test"
+            data:"",
         }
     },
+    created() {
+        this.weather_api()
+    },
+    methods: {
+        weather_api: function () {
+            fetch(url) 
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(weather) {
+                    console.log(weather);
+                    console.log(weather[0]);
+                    this.data = weather[0].timeSeries[0];
+                });
+        }
+    }
     
 })
 </script>
