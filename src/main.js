6f5990a8d8ce35a2177eab1b314be22b27c22cb5
Melops <95263124+Melonps@users.noreply.github.com>
2022-09-03 23:53:46 +0900

    [FIX] bootstrapvueを削除、bootstrapの導入
    拡張機能での動作確認済み

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from "./router";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 /* import the fontawesome core */
