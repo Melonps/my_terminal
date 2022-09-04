c9cbc036bf8b4a7e19c810065930d857ee2ca864
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-04 20:50:57 +0900

    [ADD] Googleアカウント認証を実装

---
+++ src/components/AuthGoogle.vue
@@ -0,0 +1,40 @@
+<template>
+  <button type="submit" class="btn btn-primary" v-on:click="google">Sign in with Google</button>
+</template>
+
+<script>
+  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  export default {
+    methods: {
+      google() {
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // ...
+            console.log(token)
+            console.log(user)
+          }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            console.log(errorCode)
+            console.log(errorMessage)
+            console.log(email)
+            console.log(credential)
+          });
+      }
+    }
+  };
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,8 @@
     <AuthEmailRegister></AuthEmailRegister>
     <h1>Sign in</h1>
     <AuthEmailSignIn></AuthEmailSignIn>
+    <h1>Google</h1>
+    <AuthGoogle></AuthGoogle>
     <h1>Sign out</h1>
     <AuthSignOut></AuthSignOut>
   </div>
@@ -12,6 +14,7 @@
 <script>
   import AuthEmailRegister from "@/components/AuthEmailRegister.vue";
   import AuthEmailSignIn from "../components/AuthEmailSignIn.vue";
+  import AuthGoogle from "../components/AuthGoogle.vue";
   import AuthSignOut from "../components/AuthSignOut.vue";
 
   export default {
@@ -19,7 +22,8 @@
     components: {
       AuthEmailRegister,
       AuthEmailSignIn,
-      AuthSignOut
+      AuthSignOut,
+      AuthGoogle
     },
   };
 </script>
