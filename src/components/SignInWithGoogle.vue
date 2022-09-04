57957ec6a47e8880aa6e587d85fface2686c4548
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-05 00:47:03 +0900

    [FIX] Googleサインインのコンポーネントを編集 (#11) - uidのドキュメントを作成するメソッドinitDocumentを用意した - メソッドgoogleから何故かinitDocumentを呼び出せない @Melonps

--- src/components/SignInWithGoogle.vue
+++ src/components/SignInWithGoogle.vue
@@ -7,6 +7,8 @@
 
 <script>
   import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import { doc, getDoc, setDoc } from "firebase/firestore";
+  import db from './../main'
 
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
@@ -14,21 +16,25 @@
   export default {
     data: () => ({
       userName: '',
+      uid: '',
     }),
     methods: {
-      google() {
+      google: function() {
         signInWithPopup(auth, provider)
           .then((result) => {
             // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
+            //const credential = GoogleAuthProvider.credentialFromResult(result);
+            //const token = credential.accessToken;
             // The signed-in user info.
             const user = result.user;
-            this.userName = user.displayName
+            //this.userName = user.displayName
             // ...
-            console.log(token)
-            console.log(user)
-          }).catch((error) => {
+            //console.log(token)
+            //console.log(user)
+            this.uid = user.uid
+            console.log(this.uid)
+          })
+          .catch((error) => {
             // Handle Errors here.
             const errorCode = error.code;
             const errorMessage = error.message;
@@ -40,8 +46,22 @@
             console.log(errorMessage)
             console.log(email)
             console.log(credential)
+          })
+          .then(() => {
+            this.initDocument;
           });
-      }
+      },
+      initDocument: async function() {
+        const docRef = doc(db, "users", this.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          console.log("Document data:", docSnap.data());
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+          await setDoc(docRef, {});
+        }
+      },
     }
   };
 </script>
