4e654e81df6524f33109dc61be797b0db73788c0
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-05 22:31:18 +0900

    [FIX] エラーを修正 (#2)

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,24 +1,24 @@
 <template>
     <div id="weather">
-        <p>{{ data }}</p>
-        <p>{{ area }}</p>
-        <p>{{ code }}</p>
-        <p>{{ weather_code }}</p>
-        <img :src="require(`@/assets/weather_img/${weather_code}`)"/>
+        <p>area: {{ area }}</p>
+        <p>weather_code: {{ weather_code }}</p>
+        <p>weather_img: {{ weather_img }}</p>
+        <img :src="require(`@/assets/weather_img/${weather_img}`)"/>
+        <p>error: {{error_message}}</p>
     </div>
 </template>
 
 <script>
-import weather_code from '../assets/weather_code.json'
+import weather_code_list from '../assets/weather_code.json'
 let url = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json";
 
 export default ({
     data: function () {
         return {
-            data: "",
-            area: "",
-            code: "",
-            weather_code: weather_code,
+            area: "東京地方",
+            weather_code: "100",
+            weather_img: "100.svg",
+            error_message: "No error has found.",
         }
     },
     created() {
@@ -31,10 +31,13 @@ export default ({
                     return response.json();
                 })
                 .then((weather) => {
-                    // this.data = weather[0];
                     this.area = weather[0].timeSeries[0].areas[0].area.name;
-                    this.code = weather[0].timeSeries[0].areas[0].weatherCodes[0];
-                    this.weather_code = weather_code[String(this.code)][1]
+                    this.weather_code = weather[0].timeSeries[0].areas[0].weatherCodes[0];
+                    this.weather_img = weather_code_list[String(this.weather_code)][1]
+                })
+                .catch((error) => {
+                    console.log(error)
+                    this.error_message = error
                 });
         }
     }
