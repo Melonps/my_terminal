246c18f821e3d5358f5623b2064d1124a86ecf40
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-06 00:14:22 +0900

    [FIX] UserSettingsにFirebaseを適用 (#4)

--- src/components/UserSettings.vue
+++ src/components/UserSettings.vue
@@ -1,61 +1,79 @@
 <template>
-    <div v-for="(value, key) in userData" :key="key">
-        <p> {{value}} </p>
-    </div>
-
     <div>
-        <img :src="require(`@/assets/img/${userData.background_image}.jpg`)"/>
-        <p>{{userData}}</p>
+        <img :src="require(`@/assets/img/${userSettings.bg_image}.jpg`)"/>
+        <p>{{userSettings}}</p>
     </div>
     <form>
         <h2>Update JSON</h2>
         <div class="mb-3">
             <label for="inputUserName" class="form-label">User Name</label>
-            <input v-model="userData.username" type="text" class="form-control" id="inputUserName">
+            <input v-model="userSettings.username" type="text" class="form-control" id="inputUserName">
         </div>
         <div class="mb-3">
             <label for="inputUserLocation" class="form-label">Location</label>
-            <input v-model="userData.location" type="text" class="form-control" id="inputUserLocation">
+            <input v-model="userSettings.location" type="text" class="form-control" id="inputUserLocation">
         </div>
-        <button type="button" class="btn btn-primary" v-on:click="updateData">
+        <button type="button" class="btn btn-primary" v-on:click="updateUserSettings">
             Update
         </button>
     </form>
 </template>
 
 <script>
-    const fs = require('browserify-fs');
-    import userData from '../assets/user_data/data.json'
-    console.log(userData)
+    import { getAuth } from "firebase/auth";
+    import { doc, getDoc, updateDoc } from "firebase/firestore";
+    import db from './../main'
     
     export default {
-        mounted: () => {
-            let userNameField = document.querySelector('#inputUserName');
-            userNameField.placeholder = userData.username
-            let userLocationField = document.querySelector('#inputUserLocation');
-            userLocationField.placeholder = userData.location
+        created: function() {
+            const auth = getAuth();
+            const user = auth.currentUser;
+
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                this.uid = user.uid
+                console.log(this.uid)
+            } else {
+                // No user is signed in.
+                alert("No user is signed in.")
+            }
+        },
+        mounted: function() {
+            this.fetchUserSettings(this.uid);
         },
         data: () => ({
-            userData: userData,
+            uid: "sample",
+            userSettings: {
+                username: 'hoge',
+                location: 'Osaka',
+                bg_image: 'default',
+            },
         }),
         methods: {
-            writeJSON: (newData) => {
-                fs.writeFile('../assets/user_data/data.json', JSON.stringify(newData), 'utf8', (err, data) => {
-                    if (data) {
-                        console.log(JSON.parse(data))
-                        // ここに書き込み処理を記述
-                        console.log("JSON file has been saved.");
-                    } else {
-                        console.log("An error occured while writing JSON Object to File.");
-                        return console.log(err);
+            fetchUserSettings: async function() {
+                const docRef = doc(db, "users", this.uid);
+                const docSnap = await getDoc(docRef);
+                
+                if (docSnap.exists()) {
+                    console.log("Document data:", docSnap.data());
+                    const data = docSnap.data();
+                    if (data["user_settings"]) {
+                        this.userSettings = data["user_settings"];
                     }
-                })
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                    alert("Failed to find user settings document.");
+                }
             },
-            updateData: function () {
-                const stringifiedData = JSON.stringify(this.userData)
-                console.log(stringifiedData)
-                this.writeJSON(stringifiedData)
+            updateUserSettings: async function() {
+                const docRef = doc(db, "users", this.uid);
+                
+                await updateDoc(docRef, {
+                    "user_settings": this.userSettings
+                });
             },
         },
-    }; 
+    };
 </script>
