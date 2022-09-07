df57fa04105ff678adb4d3a2ec46b9ca0fc08c33
Melops <95263124+Melonps@users.noreply.github.com>
2022-09-07 09:12:40 +0900

    [FIX] vuexでのデータ保存、表示 #13
    動くものを作りました。dev for vue必須なので入れといてください。

--- src/components/Hellovuex.vue
+++ src/components/Hellovuex.vue
@@ -1,8 +1,19 @@
 <template>
     <button type="button" class="btn btn-outline-primary" v-on:click="save">Primary</button>
-    <div><input type="text" v-model="title"/></div>
-    <div><textarea v-model="content"></textarea></div>
+    <p>タイトルを入力してください</p>
+    <div><input type="text" class="form-control-lg" v-model="title"/></div>
+    <p>内容を入力してください</p>
+    <div><textarea class="form-control" v-model="content"></textarea></div>
     <p>{{title}}</p>
+    <div class="home">
+        <p>メモ一覧</p>
+        <ui v-if="hasMemos">
+            <li v-for="memo in memos" :key="memo.id">
+                {{memo.title}}
+            </li>
+        </ui>
+        <p v-else>メモはありません</p>
+    </div>
 </template>
 
 <script>
@@ -11,8 +22,8 @@ export default {
     name: 'MemoForm',
     data() {
         return {
-            title: 'no',
-            content:'noo'
+            title: '',
+            content:''
         }
     },
     methods: {
@@ -22,6 +33,15 @@ export default {
                 content: this.content
             }
             this.$store.commit('save', memo)
+        },
+        
+    },
+    computed: {
+        memos() {
+            return this.$store.state.memos
+        },
+        hasMemos() {
+            return this.$store.state.memos.length
         }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { createStore  } from 'vuex'
 
 export default createStore({
     state: {
+        count: 0,
         memos:[]
     },
     mutations: {
         save(state, newMemo) {
+            newMemo.id = ++this.state.count
             state.memos.unshift(newMemo)
         }
     }
