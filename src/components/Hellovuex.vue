c0ede39b7f8355c3f8c84bec396810bdb09528fa
Melops <95263124+Melonps@users.noreply.github.com>
2022-09-07 12:32:42 +0900

    [FIX] メモの一覧と指定方法を二つ作成 #13
    説明必須だと思うので説明させてください.

--- src/components/Hellovuex.vue
+++ src/components/Hellovuex.vue
@@ -1,19 +1,33 @@
 <template>
+    <h1>メモ入力画面</h1>
     <button type="button" class="btn btn-outline-primary" v-on:click="save">Primary</button>
     <p>タイトルを入力してください</p>
     <div><input type="text" class="form-control-lg" v-model="title"/></div>
     <p>内容を入力してください</p>
     <div><textarea class="form-control" v-model="content"></textarea></div>
-    <p>{{title}}</p>
     <div class="home">
+        <h1>メモ一覧からインデックスを指定してメモの値を所得</h1>
         <p>メモ一覧</p>
         <ui v-if="hasMemos">
-            <li v-for="memo in memos" :key="memo.id">
-                {{memo.title}}
+            <li v-for="(memo, index) in memos" :key="memo.id">
+                {{memo.id}} {{memo.title}}
+                <button type="button" class="btn btn-outline-warning" v-on:click="sendindex(index)">show1</button>
+                <p>vuexに保存されているindexは{{index}}</p>
             </li>
+            
         </ui>
         <p v-else>メモはありません</p>
     </div>
+    <div>
+        <h1>フォームで数値指定してそのメモの値を所得</h1>
+        <input type="number" class="form-control-lg" v-model="selected_id"/>
+        <button type="button" class="btn btn-outline-warning" v-on:click="selectindex">show2</button>
+        <h1>結果表示</h1>
+        <p>{{show_id}}</p>
+        <p>{{selected_memo}}</p>
+    </div>
+    
+
 </template>
 
 <script>
@@ -23,25 +37,37 @@ export default {
     data() {
         return {
             title: '',
-            content:''
+            content: '',
+            show_id: 'none'
         }
     },
     methods: {
         save() {
+            //メモの内容をセーブする関数
+            //memoにtitleとcontentをいれ、それをsaveにコミットして保存
             let memo = {
                 title: this.title,
                 content: this.content
             }
             this.$store.commit('save', memo)
         },
-        
+        selectindex() {
+            this.show_id = this.selected_id
+        },
+        sendindex(index) {
+            this.show_id =  this.$store.getters.getMemoById(index)
+        }
     },
     computed: {
         memos() {
             return this.$store.state.memos
         },
         hasMemos() {
-            return this.$store.state.memos.length
+            return this.$store.getters.getCount
+        },
+        selected_memo() {
+            let ids = this.$store.getters.getMemoById(this.s_id)
+            return this.$store.state.memos[ids]
         }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,25 @@ import { createStore  } from 'vuex'
 
 export default createStore({
     state: {
-        count: 0,
+        //初期状態を定義
+        //countには何個目のメモかが入っていて、最新のメモはid=count、最初のメモはid=0となる。ちなみにmemosの配列にあるindexとは真逆になる
+        count: -1,
         memos:[]
     },
     mutations: {
+        //セーブ関数。memosに新たなnewmemoをいれ、newmemoidにcountを入れる
         save(state, newMemo) {
             newMemo.id = ++this.state.count
             state.memos.unshift(newMemo)
         }
+    },
+    getters: {
+        //stateの状態を返す関数を定義。
+        getCount: (state) => {
+            return state.memos.length
+        },
+        getMemoById: (state) => (id) => {
+            return state.memos.findIndex(memo => memo.id === id)
+        },
     }
 })
