990947efb5fb15a21ca4bb5b9599cd959dc5c4e3
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-07 23:04:39 +0900

    [add] 現在地の都道府県・市区を取得できるようにした (#2)

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -4,6 +4,8 @@
       Get current location
     </button>
     <p>latitidue: {{latitude}}, longitude: {{longitude}}</p>
+    <p>prefecture: {{prefecture}}</p>
+    <p>city: {{city}}</p>
   </div>
 </template>
 
@@ -13,12 +15,14 @@
   export default {
     data() {
       return {
-        latitude: 0,
-        longitude: 0
+        latitude: 0,//34.3617377,
+        longitude: 0,//132.3696928,
+        prefecture: '',
+        city: ''
       }
     },
     methods: {
-      getLocation() {
+      getLocation: function() {
         if (!navigator.geolocation) {
           alert('現在地情報を取得できませんでした。お使いのブラウザでは現在地情報を利用できない可能性があります。エリアを入力してください。')
           return
@@ -30,10 +34,34 @@
           maximumAge: 0
         }
 
-        navigator.geolocation.getCurrentPosition(this.success, this.error, options)
+        let getCurrentPosition = (options) => {
+          return new Promise((resolve) => {
+            navigator.geolocation.getCurrentPosition(resolve, this.error, options);
+          })
+        }
+
+        getCurrentPosition(options)
+          .then((position) => {
+            console.log("HELLO")
+            this.latitude = position.coords.latitude;
+            this.longitude = position.coords.longitude;
+          })
+          .then(() => {
+            // 現在地の都道府県・市区を取得
+            fetch(`http://geoapi.heartrails.com/api/json?method=searchByGeoLocation&x=${this.longitude}&y=${this.latitude}`)
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data)
+                this.prefecture = data.response.location[0].prefecture
+                this.city = data.response.location[0].city
+              }) 
+              .catch((error) => {
+                console.log(error)
+              })
+          })
       },
 
-      success(position) {
+      success (position) {
         this.latitude = position.coords.latitude
         this.longitude = position.coords.longitude
       },
@@ -53,7 +81,7 @@
           alert('現在位置が取得できませんでした')
           break
         }
-      }
+      },
     },
   };
 </script>
