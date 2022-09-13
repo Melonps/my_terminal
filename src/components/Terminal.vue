009ded9bf48dbec7dd342da9ba76ca5567f86d7f
Melops <95263124+Melonps@users.noreply.github.com>
2022-09-13 17:03:54 +0900

    [FIX] 背景画像の調節 #23
    これにならって後のコンポーネントを追加すればいいはず！

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -1,27 +1,28 @@
 <template>
-    <div class="my_terminal">
-        <div class="container">
+    <div class="whole">
+        <div class="m-0">
             <img v-if="userSettingsState.bg_image != 'default'" :src="userSettingsState.bg_image" alt="Custom Background Image" class="custom_bg"/>
             <img v-else src="@/assets/img/default.jpg" alt="Default Background Image" class="default_bg"/>
+            <div class="content">
+                <Usersettings></Usersettings>
+                <Clock></Clock>
+            </div>
         </div>
-    <div class="container"></div>
-        <Usersetting></Usersetting>
-        <Clock></Clock>
     </div>
 </template>
 
 <script>
 
-import Usersetting from "../components/UserSettings.vue";
+import Usersettings from "../components/UserSettings.vue";
 import Clock from "../components/Clock.vue";
 
 
 export default {
     name: "terminal",
     components: {
-        Usersetting,
-        Clock,
-    },
+    Usersettings,
+    Clock,
+},
     computed: {
         userSettingsState: function () {
             return this.$store.state.userSettings;
@@ -32,5 +33,23 @@ export default {
 </script>
 
 <style scoped>
+
+    .m-0{
+        position: relative;
+    }
+    /*背景画像の設定*/
+    .m-0 img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .content{
+        position: absolute;
+        top: 50%;
+        right: 0;
+        left: 0;
+        margin: auto;
+    }
 
 </style>
