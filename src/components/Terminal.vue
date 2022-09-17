19b56b06c3b4b1854a4871ba67df9bde72d7ca76
Melops <95263124+Melonps@users.noreply.github.com>
2022-09-17 22:01:33 +0900

    [ADD] ブラーの背景を追加、イイ感じに #23
    ちょっと端の方があれだけどそれはまた直す

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="whole">
         <div class="m-0">
+            <div class="bg_blur">            
+            <img v-if="userSettingsState.bg_image != 'default'" :src="userSettingsState.bg_image" alt="Custom Background Image" class="custom_bg_blur"/>
+            <img v-else src="@/assets/img/default.jpg" alt="Default Background Image" class="default_bg_blur"/>
+            </div>
+            <div class="bg_main">
             <img v-if="userSettingsState.bg_image != 'default'" :src="userSettingsState.bg_image" alt="Custom Background Image" class="custom_bg"/>
             <img v-else src="@/assets/img/default.jpg" alt="Default Background Image" class="default_bg"/>
-            <div class="content">
-                <UserSettings></UserSettings>
-                <Clock></Clock>
             </div>
-            <div v-if="!signedin_state">
-                <SignOut></SignOut>
+                <div class="content">
+                    <UserSettings></UserSettings>
+                    <Clock></Clock>
+                <div v-if="!signedin_state">
+                    <SignOut></SignOut>
+                </div>
             </div>
             
         </div>
@@ -47,7 +53,28 @@ export default {
     .m-0 img {
         width: 100%;
         height: 100%;
-        object-fit: cover;
+    }
+    .bg_blur {
+        margin: 0;
+    }
+
+    .bg_blur img{
+        position: inherit;
+        filter: blur(8px);
+        overflow: hidden;
+        width: 100%;
+
+    }
+
+    .bg_main {
+        position: absolute;
+        width: 94%;
+        height: 94%;
+        top:0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        margin: auto;
     }
 
     .content{
