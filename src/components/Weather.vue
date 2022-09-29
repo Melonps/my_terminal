c761b8151a17a8f3f3832245f137226d540d9fda
Melops <95263124+Melonps@users.noreply.github.com>
2022-09-29 23:09:36 +0900

    [FIX] コンパイルエラーの修復 #27
    かっこを消したり、いろいろ

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -81,7 +81,7 @@ export default {
             this.url = this.url.replace("130000", "140000")
         },
         weather_api: function () {
-            fetch(url) 
+            fetch(this.url) 
                 .then((response) =>{
                     return response.json();
                 })
@@ -97,5 +97,5 @@ export default {
                 });
         }
     }
-})
+}
 </script>
